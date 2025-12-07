6aac46e9744524290d23719099684058504e8bd8
Andra Stefania Stoica <87753453+andrastefania@users.noreply.github.com>
2025-12-07 12:54:32 +0200

    Initial commit

---
+++ src/HCI.AIAssistant.Api/Program.cs
@@ -0,0 +1,74 @@
+using HCI.AIAssistant.Api.Services;
+using Microsoft.Extensions.Options;
+
+using Azure.Identity;
+using HCI.AIAssistant.Api.Managers;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+var keyVaultName = builder.Configuration[$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"];
+var secretsPrefix = builder.Configuration[$"AppConfigurations{ConfigurationPath.KeyDelimiter}SecretsPrefix"];
+if (string.IsNullOrWhiteSpace(keyVaultName)) { throw new ArgumentNullException("KeyVaultName", "KeyVaultName is missing."); }
+if (string.IsNullOrWhiteSpace(secretsPrefix)) { throw new ArgumentNullException("SecretsPrefix", "SecretsPrefix is missing."); }
+
+var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential(), new CustomSecretManager(secretsPrefix));
+
+// Configure values based on appsettings.json 
+builder.Services.Configure<SecretsService>(builder.Configuration.GetSection("Secrets"));
+builder.Services.Configure<AppConfigurationsService>(builder.Configuration.GetSection("AppConfigurations"));
+// Add services to the container. 
+builder.Services.AddSingleton<ISecretsService>(
+    provider => provider.GetRequiredService<IOptions<SecretsService>>().Value);
+builder.Services.AddSingleton<IAppConfigurationsService>(
+    provider => provider.GetRequiredService<IOptions<AppConfigurationsService>>().Value);
+
+builder.Services.AddSingleton<IParametricFunctions, ParametricFunctions>();
+builder.Services.AddSingleton<IAIAssistantService, AIAssistantService>();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseCors("CORS");
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
+if (app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.EndPoint);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Key);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Id);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.IoTHubSecrets?.ConnectionString);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.KeyVaultName);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.SecretsPrefix);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?.IoTDeviceName);
+
+
+app.Run();
---
+++ src/HCI.AIAssistant.Api/Services/IAppConfigurationsService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HCI.AIAssistant.Api.Services
+{
+    public interface IAppConfigurationsService
+    {
+        public string? KeyVaultName { get; set; }
+        public string? SecretsPrefix { get; set; }
+        public string? IoTDeviceName { get; set; }
+        public string? Instruction { get; set; }
+    }
+}
